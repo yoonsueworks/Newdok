0d03e7fa3eac6732a0edd346d2b64debfb80cf1e
yoonsue <mrokr0328@gmail.com>
2023-05-26 06:02:51 +0900

    ✨ Feat: Modal Add on List, List Datas

--- components/ListedItem.js
+++ components/ListedItem.js
@@ -4,18 +4,35 @@ import { GlobalContext } from "../pages/_app";
 import Image from "next/image";
 import Tags from "./Tags";
 
-export default function ListedItem() {
-  const { setOpenModal } = useContext(GlobalContext);
+export default function ListedItem({ datas }) {
+  const { setOpenModal, modalData } = useContext(GlobalContext);
+
+  if (!datas) return;
+
+  const {
+    id,
+    name,
+    first_description,
+    second_description,
+    detail_description,
+    publication_cycle,
+    subscribe_url,
+    preview_url,
+    image_url,
+    industries,
+    interests,
+  } = datas;
+
   return (
     <li
-      onClick={() => setOpenModal(true)}
+      onClick={() => setOpenModal(datas)}
       className="bg-white p-6 h-max w-full border border-solid border-1 border-warmgray-20 rounded-2xl  cursor-pointer "
     >
       <div className="flex gap-x-4">
         <div className="w-58 h-58 rounded-full flex-shrink-0 border border-solid border-1 border-warmgray-20 relative">
           <Image
             alt=";t"
-            src="/images/thumbnail_sample.png"
+            src={image_url}
             fill
             sizes="100"
             style={{
@@ -25,12 +42,12 @@ export default function ListedItem() {
           />
         </div>
         <div>
-          <h4 className="title mb-1">name</h4>
+          <h4 className="title mb-1">{name}</h4>
           <div className="label break-keep w-full">
-            <span className="block">first_description</span>
-            <span>second_description</span>
+            <span className="block">{first_description}</span>
+            <span>{second_description}</span>
           </div>
-          <Tags />
+          <Tags tags={interests} />
         </div>
       </div>
     </li>
