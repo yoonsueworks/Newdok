202450aa650f31e78c440065f01e26df7b8e25a0
yoonsue <mrokr0328@gmail.com>
2023-05-25 22:55:31 +0900

    💄 Style

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,22 @@ export default function Home() {
   return (
     <div className="w-full h-full flex flex-col justify-between pb-20">
       <Topbar />
-      <div className="w-full h-max">
-        <div className="px-5 flex flex-col items-center justify-between space-y-31.25">
-          <div className="mb-10">
-            <Image
-              src="/images/splash.png"
-              alt="splashImage"
-              width="207"
-              height="278"
-            />
+      <div className="w-full h-full flex flex-col justify-end">
+        <div className="h-max px-5 flex flex-col items-center gap-y-[166px]">
+          <div>
+            <div className="mb-10">
+              <Image
+                src="/images/splash.png"
+                alt="splashImage"
+                width="207"
+                height="278"
+              />
+            </div>
+            <div className="headline grid text-center">
+              <div>내게 꼭 필요한 뉴스레터만</div>
+              <div>쏙쏙 뽑아줄게요!</div>
+            </div>
           </div>
-          <div className="headline">필요한 뉴스레터? 📨</div>
           <Button
             func={routeOnbooarding}
             mode="enabled"
