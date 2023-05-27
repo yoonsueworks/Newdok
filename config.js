5a8a156400e2155d4aa85bd844a360bd61a207d9
yoonsue <mrokr0328@gmail.com>
2023-05-27 18:13:48 +0900

    🙈 Chore : config, base_url set to BE deployed server

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://192.168.35.3:3001";
-// const BASE_URL = "https://newdok.shop";
+// const BASE_URL = "http://192.168.35.3:3001";
+const BASE_URL = "https://newdok.shop";
 
 const API = {
   recommend: `${BASE_URL}/newsletters?`,
