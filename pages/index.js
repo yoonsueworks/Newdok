afd040a83bbc29fb71c3dcc7dfc3daefdaaa583e
yoonsue <mrokr0328@gmail.com>
2023-06-16 00:00:18 +0900

    Test: input keyboard test

--- pages/index.js
+++ pages/index.js
@@ -256,40 +256,9 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="w-full h-full flex flex-col justify-between pb-20">
-      <Topbar />
-      <div className="w-full h-full flex flex-col justify-end">
-        <div
-          id="buttonPlacementMain"
-          className="h-max px-5 flex flex-col items-center "
-        >
-          <div>
-            <div className="mb-10">
-              <Image
-                src="/images/Newdok_Logo-motion.gif"
-                alt="뉴독 서비스 로고: 우편함"
-                id="gif"
-                className="object-cover mb-[44px]"
-                width="300"
-                height="400"
-                priority={true}
-              />
-            </div>
-            <div className="header_1 grid text-center">
-              <div>내게 꼭 필요한 뉴스레터만</div>
-              <div>쏙쏙 뽑아줄게요!</div>
-            </div>
-          </div>
-          <Button
-            func={routeUserResearch}
-            mode="enabled"
-            state={true}
-            size="big"
-            text="시작하기"
-            onClick={() => setProgress(1)}
-          />
-        </div>
-      </div>
-    </div>
+    <>
+      <div className="bg-black w-16 md:w-32 lg:w-48">splash</div>
+      <input></input>
+    </>
   );
 }
