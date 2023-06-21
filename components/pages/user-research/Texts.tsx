798316611356e5e45d1568ed3d1e4a95c5c33e52
yoonsue <mrokr0328@gmail.com>
2023-06-22 02:22:57 +0900

    Fix: optional chaining with type

--- components/pages/user-research/Texts.tsx
+++ components/pages/user-research/Texts.tsx
@@ -12,11 +12,11 @@ const UserResearchPage = ({ infos }: { infos: Infos }) => {
         <div className="w-[250px] header_2 shrink-0 mb-1">
           <span className="block">
             {string}
-            {infos.header_1_1}
+            {infos?.header_1_1}
           </span>
-          <span className="block">{infos.header_1_2}</span>
+          <span className="block">{infos?.header_1_2}</span>
         </div>
-        <div className="body_2 text-warmgray-100 mb-1">{infos.caption}</div>
+        <div className="body_2 text-warmgray-100 mb-1">{infos?.caption}</div>
       </div>
     </div>
   );
