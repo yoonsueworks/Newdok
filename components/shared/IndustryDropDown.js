ddb7bd390c7f2349f756bb3389716e3eb997fc7a
yoonsue <mrokr0328@gmail.com>
2023-09-06 15:19:17 +0900

    Fix: modify industryId name

--- components/shared/IndustryDropDown.js
+++ components/shared/IndustryDropDown.js
@@ -59,7 +59,7 @@ export default function IndustryDropDown() {
               ${selected === false ? labelCSS : selectedCSS} `}
             >
               {userDatas.industryId && !selected
-                ? industries[userDatas.industryId - 1].name
+                ? industries[userDatas.industryId - 2].name
                 : selected
                 ? selected.name
                 : "산업군 선택"}
