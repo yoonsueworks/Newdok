774ee9cebbda34856ff1ae147b8e38f6c450f23f
yoonsue <mrokr0328@gmail.com>
2023-10-26 21:16:07 +0900

    Fix: request on home

--- components/shared/Reload.js
+++ components/shared/Reload.js
@@ -1,10 +1,21 @@
 import Refresh from "icons/refresh_off.svg";
+import { useMonthlyArticles } from "service/hooks/newsletters";
+import {
+  monthlyArticlesAtom,
+  monthValueAtom,
+  dateValueAtom,
+} from "service/atoms/atoms";
 
 const Reload = () => {
+  const date = new Date();
+  const thisMonth = date.getMonth() + 1;
+  const { refetch } = useMonthlyArticles(thisMonth);
+  // 추후 일간 요청으로 변경예정
+
   return (
     <div
       className="w-fit flex gap-x-1 items-center single-14-m text-neutralgray-900 cursor-pointer"
-      onClick={() => location.reload()}
+      onClick={() => refetch()}
     >
       새로고침
       <Refresh width="20" height="20" />
