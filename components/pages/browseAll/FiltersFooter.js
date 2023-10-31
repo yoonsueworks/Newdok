da93591b0bf789b7bdfa41b570374fff8857df1c
yoonsue <mrokr0328@gmail.com>
2023-10-31 22:09:13 +0900

    Add: motion on reload, refresh btn

--- components/pages/browseAll/FiltersFooter.js
+++ components/pages/browseAll/FiltersFooter.js
@@ -1,18 +1,35 @@
+import { useState } from "react";
+
 import Refresh from "icons/refresh_off.svg";
 import { useResetRecoilState } from "recoil";
 import { browseOptionsAtom } from "service/atoms/atoms";
 const FiltersFooter = ({ onApply }) => {
   const resetBrowseOptions = useResetRecoilState(browseOptionsAtom);
 
+  const [rotation, setRotation] = useState(0);
+  const clickRefresh = () => {
+    setRotation(rotation + 360); // Set rotation to 360 degrees
+  };
+
   return (
     <div className="w-full flex gap-x-4 px-5 pb-14 pt-8">
       <button
         className="w-full text-center flex gap-x-1 justify-center single-18-b items-center"
         type="button"
-        onClick={resetBrowseOptions}
+        onClick={() => {
+          resetBrowseOptions();
+          clickRefresh();
+        }}
       >
         <div>재설정</div>
-        <Refresh width="24" height="24" />
+        <Refresh
+          width="24"
+          height="24"
+          style={{
+            transform: `rotate(${rotation}deg)`,
+          }}
+          className={`transition-all duration-500`}
+        />
       </button>
       <button
         className="w-full text-center p-5 single-24-b bg-purple-700 rounded-2xl text-white"
--- components/shared/Reload.js
+++ components/shared/Reload.js
@@ -1,24 +1,70 @@
+// import { useState } from "react";
+// import Refresh from "icons/refresh_off.svg";
+// import { useMonthlyArticles } from "service/hooks/newsletters";
+
+// const Reload = () => {
+//   const [rotation, setRotation] = useState(0);
+//   const date = new Date();
+//   const thisMonth = date.getMonth() + 1;
+//   const { refetch } = useMonthlyArticles(thisMonth);
+//   // 추후 일간 요청으로 변경예정
+
+//   const clickRefresh = () => {
+//     setRotation(rotation + 360); // Add 360 degrees to the current rotation
+//     console.log(rotation);
+//     refetch();
+//     setTimeout(() => {
+//       setRotation(0); // Reset rotation back to 0 after a delay (or after the rotation animation ends)
+//     }, 10); // Adjust the time as needed to match the rotation animation duration
+//   };
+
+//   return (
+//     <div
+//       className="w-fit flex gap-x-1 items-center single-14-m text-neutralgray-900 cursor-pointer"
+//       onClick={clickRefresh}
+//     >
+//       새로고침
+//       <Refresh
+//         width="20"
+//         height="20"
+//         style={{ transform: `rotate(${rotation}deg)` }}
+//         className={`transition-all duration-500`}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Reload;
+
+import { useState } from "react";
 import Refresh from "icons/refresh_off.svg";
 import { useMonthlyArticles } from "service/hooks/newsletters";
-import {
-  monthlyArticlesAtom,
-  monthValueAtom,
-  dateValueAtom,
-} from "service/atoms/atoms";
 
 const Reload = () => {
+  const [rotation, setRotation] = useState(0);
   const date = new Date();
   const thisMonth = date.getMonth() + 1;
   const { refetch } = useMonthlyArticles(thisMonth);
-  // 추후 일간 요청으로 변경예정
+
+  const clickRefresh = () => {
+    setRotation(rotation + 360); // Set rotation to 360 degrees
+    refetch();
+  };
 
   return (
     <div
       className="w-fit flex gap-x-1 items-center single-14-m text-neutralgray-900 cursor-pointer"
-      onClick={() => refetch()}
+      onClick={clickRefresh}
     >
       새로고침
-      <Refresh width="20" height="20" />
+      <Refresh
+        width="20"
+        height="20"
+        style={{
+          transform: `rotate(${rotation}deg)`,
+        }}
+        className={`transition-all duration-500`}
+      />
     </div>
   );
 };
