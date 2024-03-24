c22725869c8c23d330e49f22f3e881346fb3b0ba
yoonsue <mrokr0328@gmail.com>
2024-03-24 21:02:18 +0900

    Fix: page change when refresh fixed with userCurrentPlaceAtom

--- components/shared/Nav.js
+++ components/shared/Nav.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { atom, useRecoilState } from "recoil";
-import { userDatasAtom, accessTokenAtom } from "service/atoms/atoms";
+import { userCurrentPlaceAtom } from "service/atoms/atoms";
 
 import HomeOff from "icons/home_off.svg";
 import HomeOn from "icons/home_on.svg";
@@ -12,26 +12,17 @@ import ProfileOn from "icons/profile_on.svg";
 
 const Nav = () => {
   const router = useRouter();
-  const [page, setPage] = useState(null);
-
-  const hasBrowseAll = router.pathname === "/browseAll";
-  const hasUserPage = router.pathname.includes("/userPage");
-  const hasHome = router.pathname === "/home";
+  const [userCurrentPlace, setUserCurrentPlace] =
+    useRecoilState(userCurrentPlaceAtom);
 
   const clickMenu = (menu) => {
-    setPage(menu.id);
+    setUserCurrentPlace(menu.path);
     router.push(menu.path);
   };
 
   useEffect(() => {
-    if (hasBrowseAll) {
-      setPage(1);
-    } else if (hasHome) {
-      setPage(2);
-    } else if (hasUserPage) {
-      setPage(3);
-    }
-  }, [hasBrowseAll, hasHome, hasUserPage]);
+    setUserCurrentPlace(router.pathname);
+  }, [setUserCurrentPlace, router.pathname]);
 
   return (
     <div className="w-full h-fit sticky bottom-0 bg-white grid grid-cols-3 elevation-2-top pt-3.5 pb-5">
@@ -42,10 +33,12 @@ const Nav = () => {
             key={menu.id}
             onClick={() => clickMenu(menu)}
           >
-            <div>{page === menu.id ? menu.state_on : menu.state_off}</div>
+            <div>
+              {menu.path === userCurrentPlace ? menu.state_on : menu.state_off}
+            </div>
             <span
               className={
-                page === menu.id
+                menu.path === userCurrentPlace
                   ? "text-purple-700 font-bold"
                   : "text-warmgray-100 "
               }
