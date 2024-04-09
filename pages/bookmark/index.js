1f8a60ece79abe0b26e2680fa62555397e0e546f
yoonsue <mrokr0328@gmail.com>
2024-04-09 23:00:29 +0900

    feat: bookmark sample request and design

--- pages/bookmark/index.js
+++ pages/bookmark/index.js
@@ -1,68 +1,44 @@
-import React, { useRef, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
-import "swiper/css/navigation";
-import { Pagination, Navigation } from "swiper";
+import Title from "components/pages/bookmark/Title";
+import Bookmarks from "components/pages/bookmark/Bookmarks";
 
 const Bookmark = () => {
-  const userInterests = [
-    { id: 1, name: "경제 정치" },
-    { id: 2, name: "경제 정치" },
-    { id: 3, name: "경제 정치 아무것도 하지 않는 법 모던자바스크립트" },
-    { id: 4, name: "경제 정치 양꼬치김치찌개갈치조림" },
-    { id: 5, name: "경제 정치 탕수육마라탕꿔바로우마라샹궈" },
-    { id: 6, name: "경제 정치 김밥떡볶이" },
-    { id: 7, name: "경제 정치 엔터테인먼트" },
-  ];
-
   return (
     <div className="w-full">
-      <div>Bookmark</div>
-      <div className="flex">
-        <div className="icon-arrow-long-right review-swiper-button-prev">
-          이전으로\◀️
-        </div>
-
-        <Swiper
-          slidesPerView={"auto"}
-          centeredSlides={false}
-          spaceBetween={30}
-          navigation={{
-            nextEl: ".review-swiper-button-next",
-            prevEl: ".review-swiper-button-prev",
-          }}
-          //   pagination={pagination}
-          modules={[Navigation, Pagination]}
-          className="mySwiper"
-        >
-          <div className="overflow-scroll flex gap-x-2 shrink">
-            {[{ id: 0, name: "전체" }, ...userInterests].map((el) => (
-              <SwiperSlide
-                key={el.id}
-                className={`w-fit border border-black break-keep`}
-              >
-                {el.name}
-              </SwiperSlide>
-            ))}
-          </div>
-        </Swiper>
-        <div className="icon-arrow-long-left review-swiper-button-next">
-          다음으로\▶️
-        </div>
-      </div>
-      <div>
-        <div>총 개수</div>
-        <div>정렬</div>
-      </div>
+      <Title totalAmount={bookmarks.totalAmount} />
+      <Bookmarks bookmarks={bookmarks.bookmarkForMonth} />
     </div>
   );
 };
 
 export default Bookmark;
 
-// TODO:
-// 1. 북마크 카테고리 요청
-// 1. 1) 카테고리 아이디, 카테고리 명 e.g)[{interest:경제.시사, interestId:1}]
-// 2. 선택된 카테고리의 북마크된 아티클 요청 (초기 요청은 무조건 전체)
-// 2. 1) {totalAmount: 33, articles:[{month:11월, articles: [{id: 0 ,  brand: "", brandId: "", title: "", articleId: 10231, sampleText: "", brandImage:"", publishedAt:"", }, {}]}, {month:12월}]}
+const bookmarks = {
+  totalAmount: 32, // 총 32개
+  bookmarkForMonth: [
+    {
+      id: 1,
+      month: "11월", // 내림차순 정렬
+      bookmark: [
+        {
+          brandName: "데일리바이트",
+          brandId: 23,
+          articleTitle: "🤯 신입사원 시절 '최악의 실수'는?",
+          articleId: 20221,
+          sampleText:
+            "지난 11월 3일 쿠팡플레이를 통해 공개된 서바이벌 프로그램 <대학전쟁> 혹시 보셨나요? 서울대, 카이스트, 포항공대, 연세대, 고려대에 재학 중인 대학생들이 출연하는 두뇌 서바이벌로, 개인적으로는 아쉬움이 남았던 넷플릭...",
+          date: "2023-11-23",
+          imageURL: "https://newdok.shop/public/Daily Byte.png",
+        },
+        {
+          brandName: "뉴스레터 이름",
+          brandId: 2,
+          articleTitle: "🥯 전기차 배터리 시장에 찾아온 한파",
+          articleId: 38745,
+          sampleText: "최대 130자 기준",
+          date: "2023-11-23",
+          imageURL: "https://newdok.shop/public/NEWNEEK.png",
+        },
+      ],
+    },
+  ],
+};
