6d009957e3b095d37da0c1dc57c07062454e952f
yoonsue <mrokr0328@gmail.com>
2024-04-18 01:34:56 +0900

    fix: enter submit not working fixed

--- components/pages/search/SearchBar.js
+++ components/pages/search/SearchBar.js
@@ -19,14 +19,9 @@ const SearchBar = () => {
 
   const handleChange = (e) => {
     setSearchKeyword(e.target.value);
-    if (e.key === "Enter") {
-      e.preventDefault();
-      handleSubmit(onSubmit)();
-    }
   };
 
   const handleDelete = () => {
-    console.log("clicked");
     setSearchKeyword("");
   };
 
@@ -58,18 +53,11 @@ const SearchBar = () => {
               <div className="flex w-full">
                 <SearchIcon width="24" height="24" color="black" />
                 <input
-                  {...register("query", {
-                    required: {
-                      value: true,
-                      message: "뉴스레터 브랜드명, 키워드 검색",
-                    },
-                  })}
-                  type="text"
-                  value={searchKeyword}
+                  {...register("query")}
                   onChange={handleChange}
-                  maxLength="12"
-                  className="w-full"
                   placeholder="뉴스레터 브랜드명, 키워드 검색"
+                  value={searchKeyword}
+                  className="w-full"
                 />
               </div>
               {searchKeyword !== "" && (
@@ -87,6 +75,7 @@ const SearchBar = () => {
       </form>
     </div>
   );
+  a;
 };
 
 export default SearchBar;
