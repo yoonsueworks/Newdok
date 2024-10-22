42b694b21382754745f47b654a8510263f740926
yoonsue <mrokr0328@gmail.com>
2024-10-22 15:34:06 +0900

    change: favicon, loading animation

--- components/shared/HeadComp.js
+++ components/shared/HeadComp.js
@@ -14,13 +14,13 @@ export default function HeadComp() {
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="/android-icon-36x36.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="/android-icon-48x48.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
       <meta property="og:image" content="/images/og-image.png" />
